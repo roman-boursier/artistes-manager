5389720f903d1bc3cb189d1a2cb0ef938cc1e88d
Lenovo <lenovo77455@gmail.com>
2017-03-07 22:13:19 +0100

    avancement query agenda V2

--- archive-agenda.php
+++ archive-agenda.php
@@ -9,17 +9,14 @@ ini_set("xdebug.var_display_max_depth", -1);
     <div id="inner-content" class="row">
 
         <main id="main" class="large-12 medium-12 columns" role="main">
-
             <?php
-            $today = date('Ymd');
-
             $args = array(
                 'numberposts' => -1,
                 'post_type' => 'agenda'
             );
-
+            
             $liste_evenements = array();
-
+            
             $the_query = new WP_Query($args);
             if ($the_query->have_posts()) {
                 while ($the_query->have_posts()) {
@@ -27,55 +24,31 @@ ini_set("xdebug.var_display_max_depth", -1);
                     $the_query->the_post();
                     $id_evenement = get_the_id();
                     $nom_evenement = get_the_title();
-
+                   
                     // On 
                     if (have_rows('details_de_levenement')) {
                         // loop through the rows of data
                         while (have_rows('details_de_levenement')) {
                             the_row();
+
                             $date_debut = get_sub_field('date', false, false);
+
                             $mois = date_i18n("F", strtotime($date_debut));
                             $lieu = get_sub_field('lieu', false, false);
-
-                          
-                                $liste_evenements[$mois]=array();
-                         
-                                   array(
-                                    $id_evenement => array(
-                                        'titre' => $nom_evenement,
-                                        'dates'.rand()  => array(
-                                            'date_debut' => $date_debut,
-                                            'lieu' => $lieu
-                                        )
-                                    )
-                                );
-                            
+                            $lieu_address = ($lieu) ? $lieu['address'] : '';
+                           
+                            $liste_evenements[$mois][$id_evenement][$date_debut] = array(
+                                'date_de_debut' => $date_debut,
+                                'lieu' => $lieu_address
+                            );
                         }
                     }
                 }
             }
             wp_reset_query();
-
-            var_dump($liste_evenements);
-
-
-            /*
-              $liste_evenements = array(
-              'mois1' => array (
-              'id_evenement' => array(
-              'titre' => 'titre',
-              'artistes' => 'artiste 1',
-              'dates'=> array(
-              'date' => array(
-              'date_debut' => '00/00/00',
-              'date_fin' => '00/00/00',
-              'lieu' => 'lieu'
-              )
-              )
-              )
-              )
-
-              ); */
+        
+            include(locate_template('parts/loop-archive-agenda.php'));  
+            
             ?>
 
         </main> <!-- end #main -->
@@ -84,5 +57,5 @@ ini_set("xdebug.var_display_max_depth", -1);
 
 </div> <!-- end #content -->
 
-<?php //include(locate_template('parts/loop-archive-agenda.php'));   ?> 
+<?php   ?> 
 <?php get_footer(); ?>
--- parts/loop-archive-agenda.php
+++ parts/loop-archive-agenda.php
@@ -1,42 +1,31 @@
-<?php
-$preview_month = date_i18n("F", strtotime(get_field('date', $preview_post_id, false)));
-$current_month = date_i18n("F", strtotime(get_field('date', false, false)));
-?>
-
-
-<?php if ($preview_month !== $current_month || $preview_post_id == ''): ?>
-    </div>
-    </div>
+<?php foreach ($liste_evenements as $moisNom => $mois) : ?>
     <div class="row">
         <div class="columns large-2">
-            <h2><?php echo $current_month ?></h2>
+            <h2><?php echo $moisNom ?></h2>
         </div>
-        <div class="columns large-10">   
-        <?php endif; ?>
+        <div class="columns large-10">  
+            <?php foreach ($mois as $evenementId => $evenement): ?>
 
+                <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
+                    
+                    <!-------- TITRE -------------->
+                    <header class="article-header">
+                        <h3 class="title"><strong><?php echo get_the_title($evenementId); ?></strong></h3>
+                    </header> <!-- end article header -->	
+                    <?php foreach ($evenement as $date): ?>
+                    
+                        <!-------- ARTISTES & ENSEMBLE ------>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
-            <!-------- TITRE -------------->
-            <header class="article-header">
-                <h3 class="title"><strong><?php the_title(); ?></strong></h3>
-            </header> <!-- end article header -->	
+                        <!-------- DATE ----------->
+                        <small><?php echo date_i18n("d/m/y", strtotime($date['date_de_debut'])); ?></small>
 
-            <!-------- ARTISTES & ENSEMBLE ------>
-            <?php $artistes_agenda = get_field('artistes_agenda'); ?>
-            <?php if ($artistes_agenda): ?>
-                <ul class="menu simple">
-                    <?php foreach ($artistes_agenda as $artiste_agenda): ?>
-                        <?php $artiste_id = $artiste_agenda->ID; ?>
-                        <li><a href="<?php echo get_the_permalink($artiste_id); ?>"><small><?php echo get_the_title($artiste_id); ?></small></a></li>
-                        <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+                        <!-------- LIEU -------------->
+                        <small><?php echo $date['lieu']; ?></small>
 
-            <!-------- DATE ----------->
-            <small><?php the_field('date'); ?></small>
-
-            <!-------- LIEU -------------->
-            <?php $lieu = get_field('lieu'); ?>
-             <small><?php echo $lieu['address']; ?></small>
-            <hr>
-        </article> <!-- end article -->
+                    <?php endforeach; ?>
+                    <hr>
+                </article>
+            <?php endforeach; ?>
+        </div>
+    </div>
+<?php endforeach; ?>
